2459dc673c2662e765d6c5607f16432c1d4cb7a4
TheRealColbyMarquez <colbymarquez@msn.com>
2013-12-18 02:52:23 -0500

    12/18/13

---
+++ Tetris/Tetris/Tetris/SoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+namespace TetrisDemo
+{
+    public static class SoundManager
+    {
+        private static Song gameMusic;
+        public static bool IsMusicPlaying;
+
+        public static void Initialize(ContentManager content)
+        {
+            try
+            {
+                gameMusic = content.Load<Song>("gameMusic");
+            }
+            catch
+            {
+                Debug.Write("SoundManager Initialization Failed");
+            }
+        }
+
+        public static void PlayGameMusic()
+        {
+            IsMusicPlaying = true;
+            MediaPlayer.Play(gameMusic);
+
+        }
+    }
+}
